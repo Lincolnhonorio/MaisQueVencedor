f185ab5fbfdd87478ef020633c224441f45a8519
Lincoln Honorio <lincolnhonorio@hotmail.com>
2024-08-22 16:04:39 -0300

    Adicionando arquivos principais do projeto e corrigindo alterações não rastreadas

---
+++ src/test/java/com/MQVcheckmate/MaisQueVencedor/MaisQueVencedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MQVcheckmate.MaisQueVencedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisQueVencedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
